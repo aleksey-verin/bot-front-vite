6b2f50af1ecbab2b0fcf194652ff23d860b28ecd
Aleksey Verin <verevaa@gmail.com>
2024-02-27 13:48:28 +0300

    feat: added test button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
 import './App.css'
+const tg = window.Telegram.WebApp;
 
 function App() {
 
+  const onClose = () => {
+    tg.close()
+  }
+
+  useEffect(() => {
+    tg.ready();
+  }, []);
+
   return (
-    <div>Hello</div>
+    <div>
+      <div>hello</div>
+      <button onClick={onClose}>Закрыть</button>
+    </div>
   )
 }
 

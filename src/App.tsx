42975e68f652148e6056f7d6393f8d5282174f56
Aleksey Verin <verevaa@gmail.com>
2024-02-27 16:09:11 +0300

    feat: added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
 import './App.css'
-const tg = window.Telegram.WebApp;
+import Header from './components/Header/Header'
+import { Route, Routes } from 'react-router-dom'
+import ProductList from './components/ProductList/ProductList'
+import Form from './components/Form/Form'
+const tg = window.Telegram.WebApp
 
 function App() {
-
-  const onClose = () => {
-    tg.close()
-  }
+  // const onClose = () => {
+  //   tg.close()
+  // }
 
   useEffect(() => {
-    tg.ready();
-  }, []);
+    tg.ready()
+  }, [])
 
   return (
-    <div>
-      <div>hello</div>
-      <button onClick={onClose}>Закрыть</button>
-    </div>
+    <main>
+      <Header />
+      <Routes>
+        <Route index element={<ProductList />} />
+        <Route path={'form'} element={<Form />} />
+      </Routes>
+    </main>
   )
 }
 
---
+++ src/components/ProductItem/ProductItem.tsx
@@ -0,0 +1,26 @@
+import Button from "../Button/Button";
+import './ProductItem.css';
+import { Product } from '../ProductList/ProductList';
+
+const ProductItem = ({product, className, onAdd}:{product: Product, className?: string, onAdd: (product: Product) => void}) => {
+
+    const onAddHandler = () => {
+        onAdd(product);
+    }
+
+    return (
+        <div className={'product ' + className}>
+            <div className={'img'}/>
+            <div className={'title'}>{product.title}</div>
+            <div className={'description'}>{product.description}</div>
+            <div className={'price'}>
+                <span>Стоимость: <b>{product.price}</b></span>
+            </div>
+            <Button className={'add-btn'} onClick={onAddHandler}>
+                Добавить в корзину
+            </Button>
+        </div>
+    );
+};
+
+export default ProductItem;
